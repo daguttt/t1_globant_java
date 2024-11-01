39a951b9d4a104e25f67b0d16cfbbce8fced0cae
daguttt <dagutmu667@gmail.com>
2024-11-01 09:20:14 -0500

    refactor: rename exercise 4 to know what it does

--- src/exercise4/Exercise4ShopInventory.java
+++ src/exercise4/Exercise4ShopInventory.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class Exercise4 {
+public class Exercise4ShopInventory {
     private static final ArrayList<Product> productList = new ArrayList<>(
             List.of(new Product("Teclado", 500, 10),
                     new Product("Mouse", 20, 5),
@@ -18,15 +18,15 @@ public class Exercise4 {
         while (isMenuOpened) {
             String menuOptionsMessage = """
                     ********************* Menu *********************
-                    
+
                     Ingresa la opción que deseas hacer:
-                    
+
                     0. Salir.
                     1. Agregar producto.
                     2. Comprar producto.
                     3. Eliminar producto.
                     4. Mostrar inventario.
-                    
+
                     ************************************************
                     """;
             var option = InputRequester.requestString(menuOptionsMessage);
@@ -37,7 +37,8 @@ public class Exercise4 {
                     String name = InputRequester.requestString("Ingresa el nombre del producto:");
 
                     // Validate name uniqueness
-                    Optional<Product> foundProduct = productList.stream().filter(product -> product.name().equals(name)).findFirst();
+                    Optional<Product> foundProduct = productList.stream().filter(product -> product.name().equals(name))
+                            .findFirst();
 
                     if (foundProduct.isPresent()) {
                         JOptionPane.showMessageDialog(null, "El producto ya existe. Inténtalo de nuevo.");
@@ -59,9 +60,12 @@ public class Exercise4 {
 
                     // Request product to buy
                     int productIndexToBuy = InputRequester.requestAnIndexFrom(convertProductsIntoListString(),
-                            String.format("Ingresa el número del producto que quieres comprar:%n(Presiona enter para cancelar)"), true);
+                            String.format(
+                                    "Ingresa el número del producto que quieres comprar:%n(Presiona enter para cancelar)"),
+                            true);
 
-                    if (productIndexToBuy == -1) break;
+                    if (productIndexToBuy == -1)
+                        break;
 
                     Product productToBuy = productList.get(productIndexToBuy);
 
@@ -73,20 +77,25 @@ public class Exercise4 {
                     }
 
                     // Request amount to buy
-                    int amountToBuy = InputRequester.requestInteger(String.format("Ingresa la cantidad de '%s' que quieres comprar:", productToBuy.name())).orElseThrow();
+                    int amountToBuy = InputRequester.requestInteger(
+                            String.format("Ingresa la cantidad de '%s' que quieres comprar:", productToBuy.name()))
+                            .orElseThrow();
 
                     if (amountToBuy > productToBuy.stock()) {
                         JOptionPane.showMessageDialog(
                                 null,
-                                String.format("No puedes comprar '%1$d' %2$s. El stock de %2$s es '%3$d'.%n", amountToBuy, productToBuy.name(), productToBuy.stock()),
+                                String.format("No puedes comprar '%1$d' %2$s. El stock de %2$s es '%3$d'.%n",
+                                        amountToBuy, productToBuy.name(), productToBuy.stock()),
                                 "Stock insuficiente",
                                 JOptionPane.ERROR_MESSAGE);
                         break;
                     }
 
-                    var updatedProduct = new Product(productToBuy.name(), productToBuy.price(), productToBuy.stock() - amountToBuy);
+                    var updatedProduct = new Product(productToBuy.name(), productToBuy.price(),
+                            productToBuy.stock() - amountToBuy);
                     productList.set(productIndexToBuy, updatedProduct);
-                    JOptionPane.showMessageDialog(null, String.format("¡Has comprado %d %s!", amountToBuy, productToBuy.name()));
+                    JOptionPane.showMessageDialog(null,
+                            String.format("¡Has comprado %d %s!", amountToBuy, productToBuy.name()));
                 }
                 case "3" -> {
                     if (productList.isEmpty()) {
@@ -104,13 +113,15 @@ public class Exercise4 {
                     if (productToRemove.stock() > 0) {
                         JOptionPane.showMessageDialog(
                                 null,
-                                String.format("No puedes eliminar el producto: %s. Aun tiene stock disponible: '%d'.%n", productToRemove.name(), productToRemove.stock()),
+                                String.format("No puedes eliminar el producto: %s. Aun tiene stock disponible: '%d'.%n",
+                                        productToRemove.name(), productToRemove.stock()),
                                 "No puedes eliminar producto con stock",
                                 JOptionPane.ERROR_MESSAGE);
                         break;
                     }
                     productList.remove(productToRemove);
-                    JOptionPane.showMessageDialog(null, String.format("¡Producto '%s' eliminado!", productToRemove.name()));
+                    JOptionPane.showMessageDialog(null,
+                            String.format("¡Producto '%s' eliminado!", productToRemove.name()));
                 }
                 case "4" -> {
                     if (productList.isEmpty()) {
@@ -124,9 +135,9 @@ public class Exercise4 {
                     String message = String.format("""
                             ********************* Inventario *********************
                             Listado de productos:
-                            
+
                             %s
-                            
+
                             Stock total de productos: %d
                             ******************************************************""",
                             String.join("\n", convertProductsIntoListString()), totalStock);
